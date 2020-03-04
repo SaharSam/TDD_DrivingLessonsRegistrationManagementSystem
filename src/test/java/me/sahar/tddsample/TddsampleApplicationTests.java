bf1729835e5c41b3746ae41fd62184c30a4135d7
SaharSam <52576006+SaharSam@users.noreply.github.com>
2020-03-04 17:12:32 +0330

    First commit

---
+++ src/test/java/me/sahar/tddsample/TddsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sahar.tddsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddsampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
